c00c2e61dd08c44dd1cbfbcc4614dd048a209ecb
mykullma <qingyam1@uci.edu>
2023-10-17 22:12:36 -0700

    create functions page

--- _functions.py
+++ _functions.py
@@ -1,2 +1,24 @@
+from dash import html
+import dash_bootstrap_components as bootstrap
+import __utilities__ as utilities
+
 def build_page():
-    pass
+    functions = []
+
+    for function in utilities.scrape_functions().values():
+        jumbotron = bootstrap.Container(
+            [
+                html.H1(function["functionName"], className="display-5"),
+                html.P(function["functionDescription"], className="lead"),
+                html.Hr(className="my-2"),
+                html.P(function["functionText"], className="small", style={"white-space": "pre-wrap"}),
+            ],
+            fluid=True,
+            className="py-2",
+        )
+        functions.append(jumbotron)
+    
+    return bootstrap.Container(
+        children=functions,
+        fluid=True,
+    )
